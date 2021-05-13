7dbec0dd8d06d5cb2d12beb4be84a7f6ad6d1454
AGHO-DEV <aghoemmanuel.steam@gmail.com>
2021-05-13 08:07:00 +0100

    Refurbished code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,10 +50,6 @@ const Footer = () => {
                               color: "white",
                               backgroundColor: "black",
                                 padding: "15px 15px",
-                                position: "fixed",
-                                bottom: "0",
-                                left: "0",
-                                right: "0",
                                 width: "100%",
                 }}> <div className="designer-btn tooltip">DESIGNER</div> </button>
               </div>
