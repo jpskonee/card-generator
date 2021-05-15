7a3ce085dc3fa7500c8498f95d34ada67befbd8a
AGHO-DEV <aghoemmanuel.steam@gmail.com>
2021-05-15 10:12:08 +0100

    Finished design and DB working now. Waiting to extract div as jpeg/png/gif

--- src/base.js
+++ src/base.js
@@ -2,12 +2,15 @@ import firebase from "firebase";
 import "firebase/firestore";
 import "firebase/storage"
 
+
 export const app = firebase.initializeApp({
-    apiKey: "AIzaSyBe6oNaM7Oy92fclS5twBjREvs-PrcSSy8",
-    authDomain: "card-generator-bd.firebaseapp.com",
-    projectId: "card-generator-bd",
-    storageBucket: "card-generator-bd.appspot.com",
-    messagingSenderId: "966261508576",
-    appId: "1:966261508576:web:6d2a9959759b71e3c70385",
-    measurementId: "G-KEREN2T0MN"
+    apiKey: "AIzaSyAPu8baWMr9ARDbsRvjUsrrpTPiyYKkCuA",
+    authDomain: "cardgendb.firebaseapp.com",
+    projectId: "cardgendb",
+    storageBucket: "cardgendb.appspot.com",
+    messagingSenderId: "1076649912000",
+    appId: "1:1076649912000:web:ed5923cf441e9db939ef25",
+    measurementId: "G-TXJ2LMXGS4"
 });
+
+
--- src/components/Birthday-Card/BirthdayCardCreator.js
+++ src/components/Birthday-Card/BirthdayCardCreator.js
@@ -5,18 +5,18 @@ import Footer from '../Footer';
 import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
 import { app } from "../../base";
 
-const birthdayDB = app.firestore().collection("birthdayDB")
-
+const birthdayDB = app.firestore().collection("birthdayDBs");
 
 const BirthdayCardCreator = () => {
+    
+    const [doneUpload, setDoneUpload] = useState(false);
+    const [tDob, setTdob] = useState(false);
+
 
     const [cname, setCname] = useState("");
-    const [doneUpload, setDoneUpload] = useState(false);
-
     const [nickname, setNickname] = useState("");
-    const [day, setDay] = useState("");
+    const [day, setDay] = useState(0);
     const [month, setMonth] = useState("");
-    const [bdmsg, setBdmsg] = useState("");
     const [wisher, setWisher] = useState("");
     const [photo, setPhoto] = useState(null);
     
@@ -37,29 +37,26 @@ const BirthdayCardCreator = () => {
     }
 
     //pushing the img to the storage
-    const pushData = async () => {
-        if (day > 31 || day < 1) {
-            alert("Please enter a valid Day")
-        } else {
-            await birthdayDB.doc().set({
+    const pushData = async (e) => {
+        e.preventDefault();
+            if (day > 31 || day < 1) {
+                    alert("Please enter a valid Day")
+            } else {
+                await birthdayDB.doc().set({
                 name: cname,
                 nickname: nickname,
                 day: day,
                 month: month,
-                bdmsg: bdmsg,
                 wisher: wisher,
                 avatar: photo,
                 createdAt: Date.now().toLocaleString()
-            });
-            setTimeout(() => {
-                setBdmsg("")
-                setDay("")
-                setNickname("")
-                setMonth("")
-                setPhoto(null)
-                setWisher("")
-                setCname("")
-            }, 2000);
+                });
+                setDay("");
+                setNickname("");
+                setMonth("");
+                setPhoto(null);
+                setWisher("");
+                setCname("");
         }
     }
 
@@ -80,6 +77,7 @@ const BirthdayCardCreator = () => {
             alert("Please enter a valid Day")
         } else {
             setDay(`${newDay}`);
+            setTdob(true)
         }
     }
 
@@ -88,14 +86,10 @@ const BirthdayCardCreator = () => {
         setMonth(newMonth);
     }
 
-    const handleBdmsg = (event) => {
-        const newBdmsg = event.target.value;
-        setBdmsg(newBdmsg)
-    }
 
     const handleWisher = (event) => {
         const newWisher = event.target.value;
-        setWisher(newWisher)
+        setWisher(newWisher);
     }
 
 
@@ -109,9 +103,7 @@ const BirthdayCardCreator = () => {
                     <PageHeader
                         title="Card Generator"
                     />
-                </div>
-
-                       
+                </div>                    
             
                 <form>
                 
@@ -139,35 +131,35 @@ const BirthdayCardCreator = () => {
                                 
                                         
                                         <select onChange={handleMonth} value={month} required id="month" name="month">
-                                        <option selected>January</option>
-                                        <option>February</option>
-                                        <option>March</option>
-                                        <option>April</option>
-                                        <option>May</option>
-                                        <option>June</option>
-                                        <option>July</option>
-                                        <option>August</option>
-                                        <option>September</option>
-                                        <option>October</option>
-                                        <option>November</option>
-                                        <option>December</option>
+                                        <option  value="JAN" selected>January</option>
+                                        <option value="FEB">February</option>
+                                        <option value="MAR">March</option>
+                                        <option value="APRIL">April</option>
+                                        <option value="MAY">May</option>
+                                        <option value="JUNE">June</option>
+                                        <option value="JULY">July</option>
+                                        <option value="AUG">August</option>
+                                        <option value="SEP">September</option>
+                                        <option value="OCT">October</option>
+                                        <option value="NOV">November</option>
+                                        <option value="DEC">December</option>
                                         </select>
-                            </div>
-
-                            <div>
-                                 <textarea rows="5" maxlength="150" onChange={handleBdmsg} name="bdmsg" value={bdmsg} placeholder="Type your birthday message/wish...                                      *Note:Random message would be generated if left empty* "  autoCorrect ></textarea>
-                            </div>
-
+                                </div>
+                                
                             <div>
                                  <input type="text"  onChange={handleWisher} name="wisher" value={wisher} placeholder="Enter your name" autoComplete="off" />
                             </div>
                        
                            
-                    </Grid>
-                    
+                            </Grid>
+
+                            
+                    {/* Card preview sesssion start */}
                     <Grid item lg={8} md={8} sm={6} xs={12}>
                             <div className="glass-bg" style={{margin: "8px 2vw auto 2vw", position: "relative", padding: "20px auto"}}>
-                                    <div style={{marginTop: "3.5vh"
+                                    {/* preview label */}
+                                    <div style={{
+                                        marginTop: "3.5vh"
                                     }}> <span style={{
                                         background: "black",
                                         color: "aqua",
@@ -175,37 +167,154 @@ const BirthdayCardCreator = () => {
                                         fontFamily: "Press Start 2P",
                                         fontWeight: "bold",
                                         padding: "10px 20px",
-                                        borderRadius: "20px 0"
+                                        borderRadius: "5px"
                                     }}> Preview</span>  </div>
+                                    {/* preview label ends */}
 
+                                 {/* card starts    */}
                                     <div style={{
+                                        position: 'relative',
                                         margin: "4.5vh auto 5.5vh",
                                         width: "47vh",
                                         height: "47vh",
                                         border: "2px teal groove",
-                                        backgroundColor: "white",
-                                        backgroundImage: "url('https://www.transparenttextures.com/patterns/dark-brick-wall.png')",
+                                        backgroundColor: "lightgray",
+                                        backgroundImage: "url('https://www.transparenttextures.com/patterns/tex2res4.png')",
                                     }}>
-                                        <div> <img src={photo} alt="celebrant pics" height="300vh" width="250vh" /> </div>
+                                    
 
-                                        <div>{cname}</div>
-                                        <div>{nickname}</div>  
-                                        
-                                        <div> {day} {
+                                        {/* Date of birth part */}
+                                        {tDob && <div style={{
+                                            alignContent: 'left',
+                                            position: 'absolute',
+                                            border: "3px groove silver",
+                                            margin: "3px 0 0 3px",
+                                            left: "0",
+                                            top: "0"
+                                        }}>
+                                        <div style={{
+                                            display: "flex",
+                                            justifyContent: "center",
+                                            backgroundColor: "turquoise",
+                                            color: "black",
+                                        }}>
+                                                <div style={{
+                                                paddingLeft: "5px",
+                                                margin: "auto",
+                                                alignContent:"center",
+                                                fontWeight: 'bolder',
+                                                }}> {day} </div>
+                                                
+                                            <div style={{fontStyle:"italic", fontFamily: "cursive", fontSize: "8px"}}> {
                                             (day === null) ? <div>  </div> :
                                             (day === "1" || day === "21" || day === "31") ? <div> <sup> st  </sup> </div> :
                                             (day === "2" || day === "22") ? <div> <sup> nd  </sup> </div> :
                                             (day === "3" || day === "23") ? <div> <sup> rd  </sup> </div> : <div> <sup> th  </sup> </div>}
+                                                </div>
+                                                <div style={{
+                                                backgroundColor: "black",
+                                                fontSize: "10px",
+                                                fontWeight: "bold",
+                                                marginLeft: "6px",
+                                                paddingLeft: "5px",
+                                                padding: "5px",
+                                                alignContent: "center",
+                                                color: "white",
+                                             }}>{month}</div>
                                         </div>
-                                    
+                                        </div>}
+                                        {/* date of birth part ends */}
 
-                                        <div>{month}</div>
-                                        <div>{bdmsg}</div>
-                                        <div>{wisher}</div>
-                                </div>    
-                                
+                                        {/* Happy birthday label */}
+                                        <div style={{
+                                            position: "absolute", right: "0", top: "0",
+                                            margin: "4px 4px 0 0", padding: "3px 5px",
+                                            backgroundColor: "black", color: "white",
+                                            alignContent: "center", fontSize: '15px', textTransform: 'uppercase'
+                                        }}>
+                                            <div style={{
+                                                 
+                                             }}>  Happy Birthday</div> 
+                                        </div>
+                                        {/* Happy birthday label ends */}
+                                            
+                                   
+                                    {/* celebrant image part */}
+                                    <div>
+                                        <div style={{
+
+                                            margin: '9vh auto 0',
+                                            borderRadius: "0 30px",
+                                            padding: "5px 5px 2px 5px",
+                                            backgroundColor: "white",
+                                            border: 'teal solid 5px',
+                                            transform: "rotate(5deg)",
+                                            display: 'inline-block',
+                                        }}> <img src={photo} style={{borderRadius: "0 30px"}} alt="" height="120vh" width="105vh" />
+                                            </div>
+                                    </div>
+                                    {/* celebrant image part */}
+
+                                        {/* name and nickname  part */}
+                                        <div  style={{display:'inline-block',margin: "auto"}}>
+                                            {(cname.length > 1) && <div style={{
+                                                fontFamily: "monospace",
+                                                fontWeight: "bold",
+                                                textTransform: "uppercase",
+                                                borderRadius: "5px",
+                                                fontSize: "14px",
+                                                margin: "5px auto 0",
+                                                padding: "2px 20px",
+                                                color: "white",
+                                                backgroundColor: "teal"
+                                            }}>{cname}</div>}
+
+                                            {(nickname.length > 1) && <div style={{
+                                                backgroundColor: "white",
+                                                display: "inline-block",
+                                                padding: "1px 8px 3px",
+                                                borderRadius: '5px 0 0',
+                                                fontStyle: "italic",
+                                                textTransform: "lowercase",
+                                                fontSize:"11px"
+                                            }}>{nickname}</div>}
+                                        </div>
+                                        {/* name and nickname ends */}
+
+                                        {/* GIF CAKE BY THE SIDE  */}
+                                        <div style={{
+                                            position: "absolute", left: "0", bottom: "0",
+                                            margin: "0 2px 2px 0",borderRadius: '10px 0 0',
+                                            alignContent: "center",
+                                            display: "flex", justifyContent: "left"
+                                        }}>
+                                            <img src="https://thumbs.gfycat.com/CarefulUntriedIrukandjijellyfish-size_restricted.gif" alt="dancing cake" height="50px" width="50px" />
+                                        </div>
+                                        {/* GIF CAKE BY THE SIDE  */}
+                                        
+                                     {/* wishers part */}
+
+                                    {(wisher.length > 1) &&    <div  style={{
+                                            position: "absolute", right: "0", bottom: "0",
+                                            margin: "0 2px 2px 0",borderRadius: '10px 0 0',
+                                            alignContent: "center", fontSize: '11px',
+                                            display: "flex", justifyContent: "left"
+                                        }}>
+                                            <div style={{
+                                                backgroundColor: "black", color: "white", padding: "3px 5px",
+                                                
+                                            }}>FROM: </div>
+                                            <div style={{
+                                                backgroundColor: "white", color: "black", padding: "3px 5px",
+                                                fontWeight: 'bold', fontStyle: "italic", fontSize: "12px"
+                                            }}>{wisher}</div>
+                                       </div>}             
+                                     {/* wishers part ends */}
+                                    </div>
+                                    {/* card preview  ends */}
                         </div>
                     </Grid>
+                     {/* Card preview  session end */}   
                         
                     </Grid>
                 </div>          
